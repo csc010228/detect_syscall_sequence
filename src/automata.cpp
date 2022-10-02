e39fbafb442b542d0e8cf9741bd450e4e4a4a330
csc010228 <csc010228@163.com>
2022-10-02 15:20:18 +0800

    delete unnecessary cpp

--- src/automata.cpp
+++
@@ -1,9 +0,0 @@
-/*
- *
- *  automata.cpp
- *  自动机
- *  Creator:Sichao Chen
- *  Create time:2022/3/7
- *
-*/
-#include "automata.h"
--- src/dfa.cpp
+++
@@ -1,9 +0,0 @@
-/*
- *
- *  dfa.cpp
- *  确定性有限状态自动机
- *  Creator:Sichao Chen
- *  Create time:2022/2/28
- *
-*/
-#include "dfa.h"
